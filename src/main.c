430db06ecffa80f0527480e1ece5c70a815abb3f
nathandalehaskins <126778557+nathan-haskins@users.noreply.github.com>
2023-03-19 21:01:50 +0200

    Question 1.4.  init_switches Function
    Function called “init_switches” which allows the use of SW1 and SW2 on the microcontroller.

--- src/main.c
+++ src/main.c
@@ -13,10 +13,11 @@
 
 // FUNCTION DECLARATIONS -----------------------------------------------------|
 
-void main(void);                                                   //COMPULSORY
+void main(void);                        //COMPULSORY
 void display_on_LCD(uint8_t value); 	//  Question 1.1.
 void init_LEDs(void);  					//  Question 1.2.
 void display_on_LEDs(uint8_t value);	//  Question 1.3.
+void init_switches(void);				//  Question 1.4.
 
 // MAIN FUNCTION -------------------------------------------------------------|
 
@@ -55,4 +56,14 @@ void init_LEDs(void){
 void display_on_LEDs(uint8_t value){
 	GPIOB -> ODR &= ~(GPIOB -> ODR); // resets the pins of current ODR in port B
 	GPIOB -> ODR = value;
+}
+
+// Question 1.4
+void init_switches(void){
+	RCC -> AHBENR |= RCC_AHBENR_GPIOAEN; // enable clock for port A - switches
+	GPIOA-> MODER &= ~(GPIO_MODER_MODER1|
+					   GPIO_MODER_MODER2); //set pins PA0 (SW0) and PA1 (SW1) to GPIO inputs
+	GPIOA->PUPDR |= (GPIO_PUPDR_PUPDR1_0|
+					 GPIO_PUPDR_PUPDR2_0); // enable pull-up resistors: switches HIGH when open.
+
 }
